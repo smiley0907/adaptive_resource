765ac9b1720397611534d22d518a2e21be8f4321
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-08-19 13:11:10 +0530

    Update library.py

--- library.py
+++ library.py
@@ -3,3 +3,31 @@
 # ============================================================
 
 %pip install -q "qiskit>=1.1,<3" "qiskit-aer>=0.17,<0.18" pandas numpy matplotlib psutil
+
+# ============================================================
+# CELL 2: IMPORT LIBRARIES
+# ============================================================
+
+import os
+import time
+import platform
+import statistics
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import psutil
+
+import qiskit
+from qiskit import QuantumCircuit, transpile
+from qiskit.circuit.library import MCXGate
+from qiskit.circuit.library import ZGate
+from qiskit_aer import AerSimulator
+
+from IPython.display import display
+
+print("Qiskit version     :", qiskit.__version__)
+print("Platform           :", platform.platform())
+print("Python version     :", platform.python_version())
+print("Logical CPUs       :", psutil.cpu_count(logical=True))
+print("Physical CPUs      :", psutil.cpu_count(logical=False))
