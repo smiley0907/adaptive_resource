0b43914dfef50cf6fc8ecda854f15fd196f371c6
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-08-19 13:17:52 +0530

    Create before_display.py

---
+++ before_display.py
@@ -0,0 +1,18 @@
+# ============================================================
+# CELL 13: BEFORE RESULTS
+# ============================================================
+
+display(
+    baseline_df[
+        [
+            "Qubits",
+            "Gate_Count",
+            "Circuit_Depth",
+            "Shots",
+            "CPU_Threads",
+            "Median_Time_sec",
+            "Mean_Time_sec",
+            "Std_Time_sec"
+        ]
+    ]
+)
