a968fc94b934590caeb9a43dd62df42ca03b1425
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-08-19 13:10:17 +0530

    Create library.py

---
+++ library.py
@@ -0,0 +1,5 @@
+# ============================================================
+# CELL 1: INSTALL REQUIRED LIBRARIES
+# ============================================================
+
+%pip install -q "qiskit>=1.1,<3" "qiskit-aer>=0.17,<0.18" pandas numpy matplotlib psutil
